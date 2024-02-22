6fbd5f008bbbb6869b0a1c047568f675d2a895a7
luis6928 <luisponcegarrido28@gmail.com>
2024-02-22 19:36:28 +0100

    update

--- prueba/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('menuIcon').addEventListener('click', function() {
-  var menu = document.getElementById('menu');
-  if (menu.style.display === 'block') {
-      menu.style.display = 'none';
-  } else {
-      menu.style.display = 'block';
-  }
-});
